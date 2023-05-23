f3ed40691418e8099222f7dcdbbb1c69f3749f1d
Almurat98 <almurat908@gmail.com>
2023-05-23 18:49:24 -0500

    Part-8.2

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -3,14 +3,21 @@ package com.cydeo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
+   private final UserDetailsService securityService;
 
+    public SecurityConfig(UserDetailsService securityService) {
+        this.securityService = securityService;
+    }
 
-            @Bean
+    @Bean
             public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+
 
                 return http
                         .authorizeRequests()
@@ -33,6 +40,15 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/welcome")
                         .failureUrl("/login?error=true")
                         .permitAll()
+                        .and()
+                        .logout()
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login")
+                        .and()
+                        .rememberMe()
+                        .tokenValiditySeconds(120)
+                        .key("cydeo")
+                        .userDetailsService(securityService)
                         .and().build();
 
             }
