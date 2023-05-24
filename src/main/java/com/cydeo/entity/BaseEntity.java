93d49a643430fee6a3a69393bfdfffef7e7892d9
Almurat98 <almurat908@gmail.com>
2023-05-23 23:52:24 -0500

    Part-8.4

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,41 +1,33 @@
 package com.cydeo.entity;
 
 import lombok.*;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
 @MappedSuperclass
+@EntityListeners(BaseEntityListener.class)
 public class BaseEntity {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false,updatable = false)
-    private LocalDateTime insertDateTime;
+    LocalDateTime insertDateTime;
     @Column(nullable = false,updatable = false)
-    private Long insertUserId;
+    Long insertUserId;
     @Column(nullable = false)
-    private LocalDateTime lastUpdateDateTime;
+    LocalDateTime lastUpdateDateTime;
     @Column(nullable = false)
-    private Long lastUpdateUserId;
+    Long lastUpdateUserId;
     private boolean isDeleted = false;
 
 
-    @PrePersist
-    public void onPrePersist(){
-        this.insertDateTime = LocalDateTime.now();
-        this.lastUpdateDateTime= LocalDateTime.now();
-        this.insertUserId = 1L;
-        this.lastUpdateUserId= 1L;
-    }
-    @PreUpdate
-    public void onPreUpdate(){
-        this.lastUpdateUserId = 1L;
-        this.lastUpdateDateTime= LocalDateTime.now();
-    }
 
 }
--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -11,6 +11,7 @@ import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -83,7 +84,8 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public List<ProjectDTO> listAllProjectDetails() {
-        UserDTO currentUserDTO = userService.findByUserName("harold@manager.com");
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        UserDTO currentUserDTO= userService.findByUserName(username);
         User user=userMapper.convertToEntity(currentUserDTO);
 
         List<Project> list= projectRepository.findAllByAssignedManager(user);
